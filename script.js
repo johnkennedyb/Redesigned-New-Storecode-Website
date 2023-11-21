677c1c581d8dfba4236b24544423e100f1b4f4dc
Johnkennedy <113602917+johnkennedyb@users.noreply.github.com>
2023-11-20 17:07:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
 // Check if user data exists in localStorage
-var users = JSON.parse(localStorage.getItem('users')) || [];
-var authenticatedUser = JSON.parse(localStorage.getItem('authenticatedUser')) || null;
-
-
-// script.js
-
-
-
+const users = JSON.parse(localStorage.getItem('users')) || [];
+let authenticatedUser = JSON.parse(localStorage.getItem('authenticatedUser')) || null;
 
 function saveUser(username, password) {
-    var newUser = { username, password, products: [] };
+    const newUser = { username, password, products: [] };
     users.push(newUser);
     localStorage.setItem('users', JSON.stringify(users));
 }
 
-function authenticateUser() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+function createUserAndAuthenticate(username, password) {
+    saveUser(username, password);
+    authenticatedUser = { username, password, products: [] };
+    localStorage.setItem('authenticatedUser', JSON.stringify(authenticatedUser));
+    alert('Account created successfully!');
+    window.location.href = 'dashboard.html';
+}
 
-    // Check if the user is already registered
-    var existingUser = users.find(user => user.username === username && user.password === password);
+function authenticateUser() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const existingUser = users.find(user => user.username === username && user.password === password);
 
     if (existingUser) {
-        // Store the authenticated user and redirect to the dashboard page
         authenticatedUser = existingUser;
         localStorage.setItem('authenticatedUser', JSON.stringify(authenticatedUser));
         window.location.href = 'dashboard.html';
     } else {
-        // Sign up the new user, store the authenticated user, and then redirect to the dashboard page
-        saveUser(username, password);
-        authenticatedUser = { username, password, products: [] };
-        localStorage.setItem('authenticatedUser', JSON.stringify(authenticatedUser));
-        alert('Account created successfully!');
-        window.location.href = 'dashboard.html';
+        createUserAndAuthenticate(username, password);
     }
 }
 
-// Function to check if the user is authenticated before accessing the dashboard
 function checkAuthentication() {
     if (!authenticatedUser) {
         alert('Please log in to access the dashboard.');
@@ -44,61 +38,35 @@ function checkAuthentication() {
     }
 }
 
-// Function to log out the user
-function logout() {
-    localStorage.removeItem('authenticatedUser');
-    window.location.href = 'login.html';
-}
 
-// Function to save a new product to localStorage
 function saveProduct(productName, productLink) {
-    var newProduct = { productName, productLink };
+    const newProduct = { productName, productLink };
 
-    // Check if the authenticated user is available
     if (authenticatedUser && authenticatedUser.products) {
-        // Save the product to the authenticated user's products array
         authenticatedUser.products.push(newProduct);
         localStorage.setItem('authenticatedUser', JSON.stringify(authenticatedUser));
-
-        // After saving the product, display the updated list of products on the dashboard
-        displayProducts();
+        displayUserProducts(); // Call displayUserProducts instead of displayProducts
     }
 }
 
+function displayUserProducts() {
+    const dashboardContent = document.getElementById('dashboard-content');
 
-
-function displayProducts() {
-    var dashboardContent = document.getElementById('dashboard-content');
-
-    // Log the dashboardContent to the console
-    console.log('Dashboard Content:', dashboardContent);
-
-    // Check if the dashboard content element is available
     if (dashboardContent) {
-        // Clear existing content
         dashboardContent.innerHTML = '';
 
-        // Display products
         if (authenticatedUser && authenticatedUser.products && authenticatedUser.products.length > 0) {
-            // Create an unordered list to hold the products
-            var productsList = document.createElement('ul');
+            const productsList = document.createElement('ul');
 
-            // Iterate through each product and create a list item for it
             authenticatedUser.products.forEach(product => {
-                var listItem = document.createElement('li');
-
-                // Display product details
+                const listItem = document.createElement('li');
                 listItem.innerHTML = `<strong>${product.productName}:</strong> <a href="${product.productLink}" target="_blank">${product.productLink}</a>`;
-
-                // Append the list item to the products list
                 productsList.appendChild(listItem);
             });
 
-            // Append the products list to the dashboard content
             dashboardContent.appendChild(productsList);
         } else {
-            // Display a message if no products are available
-            var noProductsMessage = document.createElement('p');
+            const noProductsMessage = document.createElement('p');
             noProductsMessage.textContent = 'No products added yet.';
             dashboardContent.appendChild(noProductsMessage);
         }
@@ -109,26 +77,108 @@ function displayProducts() {
 
 // Call displayProducts on page load
 document.addEventListener('DOMContentLoaded', function () {
-    // Retrieve and display the authenticated user's information
-    var usernameDisplay = document.getElementById('username-display');
-    var userUsernameDisplay = document.getElementById('user-username');
+    const usernameDisplay = document.getElementById('username-display');
+    const userUsernameDisplay = document.getElementById('user-username');
 
-    if (authenticatedUser) {
-        usernameDisplay.textContent = authenticatedUser.username;
-        userUsernameDisplay.textContent = authenticatedUser.username;
+    if (usernameDisplay && userUsernameDisplay) {
+        if (authenticatedUser) {
+            usernameDisplay.textContent = authenticatedUser.username;
+            userUsernameDisplay.textContent = authenticatedUser.username;
+            displayUserProducts();  // Moved displayProducts call here
+        } else {
+            window.location.href = 'login.html';
+        }
+    // } else {
+    //     console.error("Error: 'username-display' or 'user-username' element not found");
+    // }
+}  //);
 
-        // Call displayProducts on page load
-        displayProducts();
-    } else {
-        // Redirect to the login page if the user is not logged in
-        window.location.href = 'login.html';
-    }
+function logout() {
+    localStorage.removeItem('authenticatedUser');
+    window.location.href = 'login.html';
+}
+
+// function saveProduct(productName, productLink) {
+//     const newProduct = { productName, productLink };
+
+//     if (authenticatedUser && authenticatedUser.products) {
+//         authenticatedUser.products.push(newProduct);
+//         localStorage.setItem('authenticatedUser', JSON.stringify(authenticatedUser));
+//         displayUserProducts();
+//     }
+// }
+
+
+
+// function displayUserProducts() {
+//     const dashboardContent = document.getElementById('dashboard-content');
+
+//     if (dashboardContent) {
+//         dashboardContent.innerHTML = '';
+
+//         if (authenticatedUser && authenticatedUser.products && authenticatedUser.products.length > 0) {
+//             const productsList = document.createElement('ul');
+
+//             authenticatedUser.products.forEach(product => {
+//                 const listItem = document.createElement('li');
+//                 listItem.innerHTML = `<strong>${product.productName}:</strong> <a href="${product.productLink}" target="_blank">${product.productLink}</a>`;
+//                 productsList.appendChild(listItem);
+//             });
+
+//             dashboardContent.appendChild(productsList);
+//         } else {
+//             const noProductsMessage = document.createElement('p');
+//             noProductsMessage.textContent = 'No products added yet.';
+//             dashboardContent.appendChild(noProductsMessage);
+//         }
+//     } else {
+//         console.error("Error: 'dashboard-content' element not found");
+//     }
+// }
+
+// 
+// document.addEventListener('DOMContentLoaded', function () {
+//     const usernameDisplay = document.getElementById('username-display');
+//     const userUsernameDisplay = document.getElementById('user-username');
+
+//     if (usernameDisplay && userUsernameDisplay) {
+//         if (authenticatedUser) {
+//             usernameDisplay.textContent = authenticatedUser.username;
+//             userUsernameDisplay.textContent = authenticatedUser.username;
+//             displayUserProducts();
+//         } else {
+//             window.location.href = 'login.html';
+//         }
+//     } else {
+//         console.error("Error: 'username-display' or 'user-username' element not found");
+//     }
 });
 
+function addProduct() {
+    // Retrieve values from the form
+    var productName = document.getElementById('product-name').value;
+    var productLink = document.getElementById('product-link').value;
 
+    // Check if both product name and link are provided
+    if (productName && productLink) {
+        // Save the product to localStorage
+        saveProduct(productName, productLink);
 
+        // Display the updated list of products on the dashboard
+        displayUserProducts(); // Corrected function name
 
-// ... existing code ...
+        // Display a success message
+        var resultMessage = document.getElementById('result-message');
+        resultMessage.textContent = 'Product added successfully!';
+        resultMessage.style.color = 'green';
 
-// Call displayProducts on page load
-displayProducts();
+        // Clear the form fields
+        document.getElementById('product-name').value = '';
+        document.getElementById('product-link').value = '';
+    } else {
+        // Display an error message if either the name or link is missing
+        var resultMessage = document.getElementById('result-message');
+        resultMessage.textContent = 'Please enter both product name and link.';
+        resultMessage.style.color = 'red';
+    }
+}
